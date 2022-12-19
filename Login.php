52202c51ca9ad06163abb042687117cc58bb13fd
MagnusHN05 <112467678+MagnusHN05@users.noreply.github.com>
2022-12-19 08:49:52 +0100

    loginside start

---
+++ Login.php
@@ -0,0 +1,29 @@
+<?php
+
+$host="localhost"
+$user="root"
+$password=""
+$db="demo"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+<div class="loginside">
+        <h1>Login</h1>
+        <form action="log-inn">
+        <p>brukernavn/passord</p>
+        <input type="text" name="Bruker" placeholder="Brukernavn">
+        <p>passord</p>
+        <input type="text" name="passord" placeholder="Passord">
+        </form>
+    </div>
+</body>
+</html>
