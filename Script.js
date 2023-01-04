b797a0c51b3564283382aa3fc4206c66bb228119
MagnusHN05 <Magnushn05@gmail.com>
2023-01-04 19:00:42 +0100

    icon on click funksjon ferdig

--- Script.js
+++ Script.js
@@ -1,10 +1,44 @@
-function changeIcon() {
-    var icon = document.getElementById("hjertet");
-    if (icon.classList.contains("fa-heart")) {
-      icon.classList.remove("fa-heart");
-      icon.classList.add("fa-solid fa-heart");
+
+function changeIconHeart() {
+    var icon = document.getElementById("heart");
+    if (icon.classList.contains("far")) {
+      icon.classList.remove("far");
+      icon.classList.add("fas");
     } else {
-      icon.classList.remove("fa-solid fa-heart");
-      icon.classList.add("fa-heart");
+      icon.classList.remove("fas");
+      icon.classList.add("far");
+    }
+  }
+  
+function changeIconSmiley() {
+    var icon = document.getElementById("smiley");
+    if (icon.classList.contains("far")) {
+      icon.classList.remove("far");
+      icon.classList.add("fas");
+    } else {
+      icon.classList.remove("fas");
+      icon.classList.add("far");
+    }
+  }
+  
+function changeIconTommelopp() {
+    var icon = document.getElementById("tommelopp");
+    if (icon.classList.contains("far")) {
+      icon.classList.remove("far");
+      icon.classList.add("fas");
+    } else {
+      icon.classList.remove("fas");
+      icon.classList.add("far");
+    }
+  }
+  
+function changeIconTommelned() {
+    var icon = document.getElementById("tommelned");
+    if (icon.classList.contains("far")) {
+      icon.classList.remove("far");
+      icon.classList.add("fas");
+    } else {
+      icon.classList.remove("fas");
+      icon.classList.add("far");
     }
   }
