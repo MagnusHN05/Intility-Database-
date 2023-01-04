e23713a389e49c244907e10a84333acd0d298a46
MagnusHN05 <Magnushn05@gmail.com>
2023-01-04 23:47:09 +0100

    loggin-icon

--- Login.php
+++ Login.php
@@ -11,10 +11,16 @@
 <div class="loginside">
         <h1>Login</h1>
         <form action="intility.php" method="post">
-            <p>brukernavn/passord</p>
-            <input type="text" name="Bruker" placeholder="Brukernavn">
-            <p>passord</p>
-            <input type="text" name="passord" placeholder="Passord"><br>
+            <div id="bruker-input">
+                <p>brukernavn</p>
+                <input type="text" name="Bruker" placeholder="Brukernavn">
+            </div>
+            <div id="passord-input">
+                <p>passord</p>
+                <input type="text" name="passord" placeholder="Passord"><br>
+                <i class="fa fa-eye">
+                <i class="fa fa-eye-slash">
+            </div>
             <div id="knapper">
                 <form action="legg_til_elever.php">
                     <button type="submit">SignUp</button>
