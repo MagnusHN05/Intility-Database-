7334d69b1b5dfec450c5e99e1f0e044e3aed8913
MagnusHN05 <Magnushn05@gmail.com>
2023-01-05 02:23:24 +0100

    fontawesome

--- rom3.php
+++ rom3.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/dfb1016c8c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="rom.css">
     <title>rom3</title>
 </head>
