cbde59208bad8e24b9912518428c4d6b3bd0e103
MagnusHN05 <Magnushn05@gmail.com>
2023-01-05 02:20:24 +0100

    rom

---
+++ rom4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rom4</title>
+    <link rel="stylesheet" href="rom.css">
+</head>
+<body>
+<div class="iconer">
+    <i id="smiley" class="far fa-face-smile" onclick="changeIconSmiley()"></i>
+    <i id="heart" class="far fa-heart" onclick="changeIconHeart()"></i>
+    <i id="tommelopp" class="far fa-thumbs-up" onclick="changeIconTommelopp()"></i>
+    <i id="tommelned" class="far fa-thumbs-down" onclick="changeIconTommelned()"></i>
+</div>
+<script src="script.js"></script>
+</body>
+</html>
