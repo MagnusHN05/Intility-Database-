d69dcc0d8a436ee70bc33e60f6cc26c3ebe1ee7f
MagnusHN <magnushn05@gmail.com>
2023-01-05 18:53:30 +0100

    reaksjons teller

--- Script.js
+++ Script.js
@@ -43,6 +43,43 @@ function changeIconTommelned() {
     }
   }
 
+const heart = document.getElementById("heart");
+const countheart = document.getElementById('heart');
+let count1 = 0; 
+
+heart.addEventListener('click', function(){
+  count1 +=1;
+  countheart.innerHTML = count1;
+})
+
+const smiley = document.getElementById("smiley");
+const countsmiley = document.getElementById('smiley');
+let count2 = 0; 
+
+smiley.addEventListener('click', function(){
+  count2 +=1;
+  countsmiley.innerHTML = count2;
+})
+
+const tommelopp = document.getElementById("tommelopp");
+const counttommelopp = document.getElementById('tommelopp');
+let count3 = 0; 
+
+tommelopp.addEventListener('click', function(){
+  count3 +=1;
+  counttommelopp.innerHTML = count3;
+})
+
+const tommelned = document.getElementById("tommelned");
+const counttommelned = document.getElementById('tommelned');
+let count4 = 0; 
+
+tommelned.addEventListener('click', function(){
+  count4 +=1;
+  counttommelned.innerHTML = count4;
+})
+
+  /*
 let heart = 0;
 let smiley = 0;
 let tommelopp = 0;
@@ -52,3 +89,4 @@ function countClicks1(){
     heart++;
     document.getElementById('heart').innerHTML = heart;
 }
+*/
